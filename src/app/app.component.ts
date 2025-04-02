2973866a598fb0dafe22dd46016de1ab564baf44
Marcin Szafert <marcin.szafert@xebia.com>
2025-04-02 11:52:06 +0200

    UPDATE cleanup startup files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles: [],
 })
 export class AppComponent {
-  title = 'tax';
+  title = 'Tax component';
 }
