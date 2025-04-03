ce66032ab61fc8a65f97aa0374879a5298782ed9
Marcin Szafert <marcin.szafert@xebia.com>
2025-04-03 17:52:13 +0200

    FIX pocketbase import

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import Pocketbase from 'Pocketbase';
+import Pocketbase from 'pocketbase';
 import { BehaviorSubject } from 'rxjs';
 import { UserModel } from '../../interfaces/user-model';
 import { environment } from '../../../environments/environment.development';
