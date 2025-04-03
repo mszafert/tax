01d769068acd8d91c4f6748c55c2934cebf769fc
Marcin Szafert <mszafert@gmail.com>
2025-04-03 18:31:51 +0200

    LoginLayoutComponent (#1)
    * ADD placeholder of LoginLayout
    
    * ADD login layout
    
    * FIX pocketbase import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginLayoutComponent } from './components/layouts/login-layout/login-layout.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent, pathMatch: 'full' },
+  {
+    path: '',
+    component: LoginLayoutComponent,
+    children: [
+      { path: '', component: LoginComponent, pathMatch: 'full' },
+    ]
+  },
   { path: 'home', component: HomeComponent },
 ];
