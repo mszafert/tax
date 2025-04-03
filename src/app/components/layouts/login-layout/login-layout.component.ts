f0024062d8685fbe35b0a6e8b6a0532ecf365ce1
Marcin Szafert <marcin.szafert@xebia.com>
2025-04-03 17:51:20 +0200

    ADD login layout

--- src/app/components/layouts/login-layout/login-layout.component.ts
+++ src/app/components/layouts/login-layout/login-layout.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login-layout',
-  imports: [],
-  templateUrl: './login-layout.component.html'
+  imports: [RouterOutlet],
+  templateUrl: './login-layout.component.html',
+  styleUrl: './login-layout.component.css',
 })
-export class LoginLayoutComponent {
-
-}
+export class LoginLayoutComponent {}
--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig, presetWind4, transformerDirectives } from "unocss";
+import {
+  defineConfig,
+  presetWind4,
+  transformerDirectives,
+} from "unocss";
 
 export default defineConfig({
   content: {
     filesystem: ['**/*.{html,ts}'],
   },
   presets: [presetWind4()],
+  rules: [
+    ['inset-shadow-2xl', {'box-shadow': 'inset 0 0 100px 0 rgba(0,0,0,.25)'}],
+  ],
   transformers: [transformerDirectives()],
 })
